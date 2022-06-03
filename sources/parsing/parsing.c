756eb767625dcef268f1c27307477c09cb0d7dd6
Killian Mammeri <kmammeri@z1r12p1.42lyon.fr>
2022-06-03 16:05:21 +0200

    test merge 4

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -6,8 +6,12 @@
 /*   By: kmammeri <kmammeri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 15:53:31 by kmammeri          #+#    #+#             */
-/*   Updated: 2022/06/03 15:53:48 by kmammeri         ###   ########.fr       */
+/*   Updated: 2022/06/03 16:04:22 by kmammeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+void	ft_parsing(void)
+{
+}
