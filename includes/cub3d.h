b9a0357b426ef9d9a0ad229e613540ff00e68d05
Loris Puchol <lpuchol@z1r12p3.42lyon.fr>
2022-06-03 18:20:49 +0200

    change poste

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmammeri <kmammeri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpuchol <lpuchol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 13:08:21 by kmammeri          #+#    #+#             */
-/*   Updated: 2022/06/03 17:09:41 by kmammeri         ###   ########.fr       */
+/*   Updated: 2022/06/03 18:18:39 by lpuchol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 # define CUB3D_H
 
 # include "../libft/libft.h"
+# include "../minilibx/mlx.h"
 # include <math.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 
-
-typedef struct s_data
+typedef struct s_graph
 {
 	void	*sprite_n;
 	void	*sprite_s;
@@ -28,17 +28,18 @@ typedef struct s_data
 	void	*sprite_w;
 	int		floor;
 	int		ceiling;
-}				t_data;
+}				t_graph;
 
 typedef struct s_player
 {
 	float	x;
 	float	y;
+	float	dir;
 }				t_player;
 
 typedef struct s_game
 {
-	t_data	*data;
+	t_graph	*graph;
 	char	**map;
 	void	*mlx_ptr;
 	void	*mlx_window;
