98a755675fa0c7ec123077631095cdc1c936276f
Killian Mammeri <kmammeri@z1r12p1.42lyon.fr>
2022-06-03 17:10:34 +0200

    new structures

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: kmammeri <kmammeri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 13:08:21 by kmammeri          #+#    #+#             */
-/*   Updated: 2022/06/03 15:24:17 by kmammeri         ###   ########.fr       */
+/*   Updated: 2022/06/03 17:09:41 by kmammeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,32 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+
+
+typedef struct s_data
+{
+	void	*sprite_n;
+	void	*sprite_s;
+	void	*sprite_e;
+	void	*sprite_w;
+	int		floor;
+	int		ceiling;
+}				t_data;
+
+typedef struct s_player
+{
+	float	x;
+	float	y;
+}				t_player;
+
+typedef struct s_game
+{
+	t_data	*data;
+	char	**map;
+	void	*mlx_ptr;
+	void	*mlx_window;
+	int		w_height;
+	int		w_width;
+}				t_game;
 
 #endif
