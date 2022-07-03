100dee285be4cdaaf3c8eaa39a207d1a814ed112
Killian Mammeri <kmammeri@z1r12p5.42lyon.fr>
2022-07-03 23:04:37 +0200

    restart raycast

--- sources/execution/init_ray.c
+++ sources/execution/init_ray.c
@@ -6,7 +6,7 @@
 /*   By: kmammeri <kmammeri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 20:35:34 by kmammeri          #+#    #+#             */
-/*   Updated: 2022/07/02 02:50:11 by kmammeri         ###   ########lyon.fr   */
+/*   Updated: 2022/07/03 23:03:05 by kmammeri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,151 +22,103 @@
 // 	i++;
 // }
 
-void	ft_find_wall(t_game *game, int ray, float angle)
+void	ft_display_rays(t_game *game, int ray, float angle)
 {
-	float		pt_tmp_v[2];
-	float		pt_tmp_h[2];
-	float		tangle;
 	t_rectangle	rect;
 
-	tangle = angle;
-	if (angle < M_PI && angle > M_PI_2)
-		tangle = angle + M_PI_2;
-	if (angle < M_PI * 2 && angle > M_PI_2 * 3)
-		tangle = angle + M_PI_2;
-	while (game->ray[ray].wall[0] == 0 && angle < M_PI * 2 && angle > M_PI)
+	game->ray->dist[0] = cosf(fabsf(angle - (float)game->player->dir)) * sqrtf((game->player->x - game->ray[ray].pt_v[0]) * (game->player->x - game->ray[ray].pt_v[0]) + (game->player->y - game->ray[ray].pt_v[1]) * (game->player->y - game->ray[ray].pt_v[1]));
+	game->ray->dist[1] = cosf(fabsf(angle - (float)game->player->dir)) * sqrtf((game->player->x - game->ray[ray].pt_h[0]) * (game->player->x - game->ray[ray].pt_h[0]) + (game->player->y - game->ray[ray].pt_h[1]) * (game->player->y - game->ray[ray].pt_h[1]));
+	if (angle >= game->player->dir - game->r_h && angle <= game->player->dir + game->r_h)
 	{
-		pt_tmp_v[0] = game->ray[ray].pt_v[0];
-		pt_tmp_v[1] = floorf(game->ray[ray].pt_v[1]);
-		if (angle != M_PI_2 && angle != 3 * M_PI_2)
-		{
-			// dprintf(2, "test1 1\n");
-			game->ray[ray].pt_v[0] = pt_tmp_v[0] + fabsf(game->ray[ray].pt_v[1] - pt_tmp_v[1]) * tanf(tangle + M_PI_2);
-			// dprintf(2, "test2 1\n");
-			game->ray[ray].pt_v[1] = pt_tmp_v[1];
-			if (game->map[(int)floorf(game->ray[ray].pt_v[1]) - 1][(int)floorf(game->ray[ray].pt_v[0])] == '1' || game->ray[ray].pt_v[1] - 1 <= 0)
-				game->ray[ray].wall[0] = 1;
-			else
-				game->ray[ray].pt_v[1] -= 1;
-		}
-		else 
-		{
-			game->ray[ray].pt_v[1] = MAXFLOAT;
-			game->ray[ray].wall[0] = 1;
-			dprintf(2, "test1\n");
-		}
-	}
-	while (game->ray[ray].wall[0] == 0 && angle > 0 && angle < M_PI)
-	{
-		pt_tmp_v[0] = game->ray[ray].pt_v[0];
-		pt_tmp_v[1] = floorf(game->ray[ray].pt_v[1]) + 1;
-		if (angle != M_PI_2 && angle != 3 * M_PI_2)
-		{
-			// dprintf(2, "test1 2 ray == %d\n", ray);
-			game->ray[ray].pt_v[0] = pt_tmp_v[0] + fabsf(game->ray[ray].pt_v[1] - pt_tmp_v[1]) * tanf(tangle);
-			// dprintf(2, "test2 2 ray == %d\n", ray);
-			game->ray[ray].pt_v[1] = pt_tmp_v[1];
-			if (!game->map[(int)game->ray[ray].pt_v[1] + 1] || game->map[(int)floorf(game->ray[ray].pt_v[1]) + 1][(int)floorf(game->ray[ray].pt_v[0])] == '1')
-				game->ray[ray].wall[0] = 2;
-			else
-				game->ray[ray].pt_v[1] = pt_tmp_v[1];
-			// dprintf(2, "test2 2 ray == %d\n", ray);
-		}
-		else 
-		{
-			game->ray[ray].pt_v[1] = MAXFLOAT;
-			game->ray[ray].wall[0] = 2;
-			dprintf(2, "test2\n");
-		}
-	}
-	while (game->ray[ray].wall[1] == 0 && angle > M_PI_2 && angle < 3 * M_PI_2)
-	{
-		pt_tmp_h[0] = floorf(game->ray[ray].pt_h[0]);
-		pt_tmp_h[1] = game->ray[ray].pt_h[1];
-		if (angle != M_PI_2 && angle != 3 * M_PI_2)
-		{
-			game->ray[ray].pt_h[0] = pt_tmp_h[0];
-			// dprintf(2, "test1 3\n");
-			game->ray[ray].pt_h[1] = pt_tmp_h[1] + fabsf(game->ray[ray].pt_h[0] - pt_tmp_h[0]) * tanf(tangle);
-			// dprintf(2, "test2 3\n");
-			if (game->map[(int)floorf(game->ray[ray].pt_h[1])][(int)floorf(game->ray[ray].pt_h[0]) - 1] == '1' || game->ray[ray].pt_h[0] - 1 <= 0)
-				game->ray[ray].wall[1] = 1;
-			else
-				game->ray[ray].pt_h[0] -= 1;
-		}
-		else 
-		{
-			game->ray[ray].pt_h[0] = MAXFLOAT;
-			game->ray[ray].wall[1] = 1;
-			dprintf(2, "test3\n");
-		}
-	}
-	while (game->ray[ray].wall[1] == 0 && ((angle < M_PI_2 && angle >= 0) || (angle > M_PI_2 * 3 && angle <= 2 * M_PI)))
-	{
-		pt_tmp_h[0] = floorf(game->ray[ray].pt_h[0]) + 1;
-		pt_tmp_h[1] = game->ray[ray].pt_h[1];
-		if (angle != M_PI_2 && angle != 3 * M_PI_2)
-		{
-			game->ray[ray].pt_h[0] = pt_tmp_h[0];
-			// dprintf(2, "test1 4\n");
-			game->ray[ray].pt_h[1] = pt_tmp_h[1] + fabsf(game->ray[ray].pt_h[0] - pt_tmp_h[0]) * tanf(tangle);
-			// dprintf(2, "test2 4\n");
-			if (game->map[(int)floorf(game->ray[ray].pt_h[1])][(int)floorf(game->ray[ray].pt_h[0]) + 1] == '1' || !game->map[(int)game->ray[ray].pt_h[1]][(int)game->ray[ray].pt_h[0]])
-				game->ray[ray].wall[1] = 2;
-			else
-				game->ray[ray].pt_h[0] = pt_tmp_h[0];
-		}
-		else 
-		{
-			game->ray[ray].pt_h[0] = MAXFLOAT;
-			game->ray[ray].wall[1] = 1;
-			dprintf(2, "test4\n");
-		}
-	}
-	if (angle != (float)game->player->dir)
-	{
-		game->ray->dist[0] = cosf(fabsf(angle - (float)game->player->dir)) * sqrtf((game->player->x - game->ray[ray].pt_v[0]) * (game->player->x - game->ray[ray].pt_v[0]) + (game->player->y - game->ray[ray].pt_v[1]) * (game->player->y - game->ray[ray].pt_v[1]));
-		game->ray->dist[1] = cosf(fabsf(angle - (float)game->player->dir)) * sqrtf((game->player->x - game->ray[ray].pt_h[0]) * (game->player->x - game->ray[ray].pt_h[0]) + (game->player->y - game->ray[ray].pt_h[1]) * (game->player->y - game->ray[ray].pt_h[1]));
+		dprintf(2, "test4 dist hor == %f\n", game->ray->dist[0]);
+		dprintf(2, "test4 dist vert == %f\n", game->ray->dist[1]);
 	}
 	game->ray->dist_first_wall = game->ray->dist[0];
+	game->ray->first_wall = game->ray->wall[0];
 	if (game->ray->dist[0] > game->ray->dist[1])
-		game->ray->dist_first_wall = game->ray->dist[1];
-	rect.tl.x = ray;
-	rect.tl.y = (int)(game->w_he * 0.5 - 10000 / (game->ray->dist_first_wall * 100));
-	if (rect.tl.y < 0 || rect.tl.y > game->w_he)
 	{
-		// exit (0);
-		// dprintf(2, "impact vert  x = %f | y = %f\n dist = %f\n", game->ray[ray].pt_v[0], game->ray[ray].pt_v[1], game->ray->dist[0]);
-		// dprintf(2, "impact hor  x = %f | y = %f\n dist = %f\n", game->ray[ray].pt_h[0], game->ray[ray].pt_h[1], game->ray->dist[1]);
-		rect.tl.y = 10;
+		game->ray->first_wall = game->ray->wall[1];
+		game->ray->dist_first_wall = game->ray->dist[1];
 	}
+	if (angle >= game->player->dir - game->r_h && angle <= game->player->dir + game->r_h)
+		dprintf(2, "test4 dist first wall == %f\n", game->ray->dist_first_wall);
+	rect.tl.x = ray;
+	rect.tl.y = (int)(game->w_he * 0.5 - 15000 / (game->ray->dist_first_wall * 100));
+	if (rect.tl.y < 0 || rect.tl.y > game->w_he)
+		rect.tl.y = 10;
 	rect.br.x = ray;
-	rect.br.y = (int)(game->w_he * 0.5 + 10000 / (game->ray->dist_first_wall * 100));
+	rect.br.y = (int)(game->w_he * 0.5 + 15000 / (game->ray->dist_first_wall * 100));
 	if (rect.br.y < 0 || rect.br.y > game->w_he)
 		rect.br.y =  game->w_he - 10;
-	ft_rectangle(game->screen, rect, 0x00FFFFFF);
+	if (game->ray->first_wall == 1)
+		ft_rectangle(game->screen, rect, 0x00FFFFFF);
+	if (game->ray->first_wall == 2)
+		ft_rectangle(game->screen, rect, 0x00F5F5F5);
+	if (game->ray->first_wall == 3)
+		ft_rectangle(game->screen, rect, 0x00F0F0F0);
+	if (game->ray->first_wall == 4)
+		ft_rectangle(game->screen, rect, 0x00EAEAEA);
+}
+
+void	ft_find_wall(t_game *game, int ray, float angle, int init[2])
+{
+	while (game->ray[ray].first_wall == 0)
+	{
+		if (angle > 0 && angle < M_PI && game->ray[ray].dist[0] == 0)
+		{
+			game->ray[ray].wall[0] = 1;
+			game->ray[ray].pt_v[1] = game->ray[ray].pt_v[1] + init[0] * tanf(angle);
+		}
+		else if (game->ray[ray].dist[0] == 0)
+		{
+			game->ray[ray].wall[0] = 2;
+			game->ray[ray].pt_v[1] = game->ray[ray].pt_v[1] + init[0] * tanf(angle);
+			
+		}
+		init[0] = 1;
+		if (game->ray[ray].pt_v[1] > 0 && game->ray[ray].pt_v[0] > 0 && game->map[(int)game->ray[ray].pt_v[1]] && game->map[(int)game->ray[ray].pt_v[1]][(int)game->ray[ray].pt_v[0]])
+		{
+			if (angle > M_PI_2 && angle < 3 * M_PI_2)
+			{
+				if (game->map[(int)game->ray[ray].pt_v[1]][(int)game->ray[ray].pt_v[0]] == '1' && game->ray[ray].dist[0] == 0)
+					game->ray[ray].dist[0] = sqrtf((game->player->x - game->ray[ray].pt_v[0]) * (game->player->x - game->ray[ray].pt_v[0]) + (game->player->y - game->ray[ray].pt_v[1]) * (game->player->y - game->ray[ray].pt_v[1]));
+			}
+			else
+			{
+				if (game->map[(int)game->ray[ray].pt_v[1]][(int)game->ray[ray].pt_v[0]] == '1' && game->ray[ray].dist[0] == 0)
+					game->ray[ray].dist[0] = sqrtf((game->player->x - game->ray[ray].pt_v[0]) * (game->player->x - game->ray[ray].pt_v[0]) + (game->player->y - game->ray[ray].pt_v[1]) * (game->player->y - game->ray[ray].pt_v[1]));
+			}
+		}
+		if (angle > 0 && angle < M_PI)
+			game->ray[ray].pt_v[0] += 1;
+		else
+		 	game->ray[ray].pt_v[0] -= 1;
+		if (angle > 0 && angle < M_PI)
+		
+	}
+
 	// dprintf(2, "dist fisrt wall == %f\n", game->ray->dist_first_wall);
 }
 
 void	ft_init_ray(t_game *game, int ray, float angle)
 {
-	// if ()
-	// 	dprintf(2, "up\n");
-	// if (game->player->dir > 0 && game->player->dir < M_PI)
-	// 	dprintf(2, "down\n");
-	// if ((game->player->dir < M_PI_2 && game->player->dir >= 0) || (game->player->dir > M_PI_2 * 3 + 0.000001 && game->player->dir <= 2 * M_PI))
-	// 	dprintf(2, "right\n");
-	// if (game->player->dir > M_PI_2 + 0.00001 && game->player->dir < 3 * M_PI_2)
-	// 	dprintf(2, "left\n");
+	int	init[2];
+
 	game->ray[ray].dist[0] = 0;
 	game->ray[ray].dist[1] = 0;
 	game->ray[ray].wall[0] = 0;
 	game->ray[ray].wall[1] = 0;
-	game->ray[ray].pt_v[0] = game->player->x;
+	game->ray[ray].pt_v[0] = floorf(game->player->x) + 1;
 	game->ray[ray].pt_v[1] = game->player->y;
 	game->ray[ray].pt_h[0] = game->player->x;
-	game->ray[ray].pt_h[1] = game->player->y;
-	ft_find_wall(game, ray, angle);
+	game->ray[ray].pt_h[1] = floorf(game->player->y) + 1;
+	if (angle > M_PI_2 && angle < 3 * M_PI_2)
+		game->ray[ray].pt_v[0] -= 1;
+	if (angle > M_PI && angle < 2 * M_PI)
+		game->ray[ray].pt_h[1] -= 1;
+	init[0] = game->player->x - game->ray[ray].pt_v[0];
+	init[1] = game->player->y - game->ray[ray].pt_h[1];
+	ft_find_wall(game, ray, angle,  init);
 }
 
 void	ft_cast_ray(t_game *game)
@@ -191,7 +143,7 @@ void	ft_cast_ray(t_game *game)
 		if (game->player->dir > 3 * M_PI_2 - game->rot && game->player->dir < 3 * M_PI_2 + game->rot)
 			game->player->dir = 3 * M_PI_2;
 		ft_init_ray(game, i, angle);
-		// ft_get_next_pt_x(game, i, angle);
+		// ft_display_rays(game, i, angle);
 		angle += game->r_h;
 		i++;
 	}
