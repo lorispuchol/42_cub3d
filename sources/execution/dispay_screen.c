94cc2d24e817cf3fa2da2c650d1bdc3e8b7c0009
Killian Mammeri <kmammeri@z1r12p5.42lyon.fr>
2022-07-20 12:29:34 +0200

    floor still in progress

--- sources/execution/dispay_screen.c
+++ sources/execution/dispay_screen.c
@@ -6,7 +6,7 @@
 /*   By: kmammeri <kmammeri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 22:59:17 by kmammeri          #+#    #+#             */
-/*   Updated: 2022/07/16 04:00:39 by kmammeri         ###   ########lyon.fr   */
+/*   Updated: 2022/07/20 12:16:16 by kmammeri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ void	ft_floor(t_game *g)
 		g->graph->ground->img = mlx_xpm_file_to_image(g->mlx_ptr, "./sprites/dirt.xpm", &g->graph->ground->width, &g->graph->ground->height);
 	if (!g->graph->ground->addr)
 		g->graph->ground->addr = mlx_get_data_addr(g->graph->ground->img, &g->graph->ground->b_p_pix, &g->graph->ground->l_len, &g->graph->ground->endian);
-	offset[0] = (g->player->x - floorf(g->player->x)) * g->graph->ground->width;
-	offset[1] = (g->player->y - floorf(g->player->y)) * g->graph->ground->height;
+	offset[0] = (g->player->x - floorf(g->player->x));
+	offset[1] = (g->player->y - floorf(g->player->y));
 	// offset[0] = cosf(g->player->dir) * g->graph->ground->width;
 	// offset[1] = sinf(g->player->dir) * g->graph->ground->height;
 	xy[1] = g->w_he - 1;
@@ -48,7 +48,7 @@ void	ft_floor(t_game *g)
 				y_2 = 1;
 			i = 0;
 		}
-		color[1] = (int)(((float)i / (float)y_2) * g->graph->ground->height) + sinf(g->player->tilt) * g->graph->ground->height;
+		color[1] = (int)(((float)(i + offset[1] * 10) / (float)y_2) * g->graph->ground->height);
 		while (color[1] >= g->graph->ground->height)
 			color[1] -= g->graph->ground->height;
 		while (color[1] < 0)
@@ -56,7 +56,7 @@ void	ft_floor(t_game *g)
 		xy[0] = 0;
 		while (xy[0] < g->w_wi)
 		{
-			color[0] = (int)(((float)(xy[0] - (g->w_wi - x_len) * 0.5) / (float)x_len) * g->graph->ground->width);
+			color[0] = (int)(((float)(xy[0] - (g->w_wi - x_len) * 0.5) / (float)x_len) * g->graph->ground->width) + (sinf(g->player->dir - g->fov)) *g->graph->ground->width;
 			while (color[0] >= g->graph->ground->width)
 				color[0] -=  g->graph->ground->width;
 			while (color[0] < 0)
@@ -96,7 +96,7 @@ void	ft_night(t_game *g)
 			ft_set_pix(g->screen, xy[0], xy[1], ft_get_color(g->graph->sky, color[0], color[1]));
 		}
 	}
-	ft_floor(g);
+	// ft_floor(g);
 	// back.tl.x = 0;
 	// back.tl.y = g->w_he * 0.5 + (int)(sinf(g->player->tilt) * g->w_he * 0.5);
 	// back.br.x = g->w_wi;
--- sources/execution/print_ray.c
+++ sources/execution/print_ray.c
@@ -6,7 +6,7 @@
 /*   By: kmammeri <kmammeri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 18:11:36 by lorispuchol       #+#    #+#             */
-/*   Updated: 2022/07/16 02:39:29 by kmammeri         ###   ########lyon.fr   */
+/*   Updated: 2022/07/20 12:25:56 by kmammeri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,39 @@ int	x_col(t_game *g, int x, t_data *sprite)
 				+ g->ray[x].pt_impact_y
 				- floorf(g->ray[x].pt_impact_y)) * sprite->width);
 	return (color_x);
+}
+
+void	ft_print_floor_texture(t_game *g, int x, int y)
+{
+	long double	angle;
+	long double	true_dist;
+	long double	dist;
+	int			color[2];
+
+	if (!g->graph->ground->img)
+		g->graph->ground->img = mlx_xpm_file_to_image(g->mlx_ptr, "./sprites/dirt.xpm", &g->graph->ground->width, &g->graph->ground->height);
+	if (!g->graph->ground->addr)
+		g->graph->ground->addr = mlx_get_data_addr(g->graph->ground->img, &g->graph->ground->b_p_pix, &g->graph->ground->l_len, &g->graph->ground->endian);
+	if (g->key->night_mode == 0)
+		return ;
+	angle = fabsl(g->player->dir - g->ray[x].angle);
+	while (y < g->w_he - 1)
+	{
+		dist = y * 0.5 * g->graph->ground->height / (y - 0.5 * g->w_he);
+		true_dist = dist / cosf(angle);
+		color[0] = (g->player->x + cosf(M_PI - g->player->dir) * true_dist) / g->graph->ground->width;
+		color[1] = (g->player->y - sinf(M_PI + g->player->dir) * true_dist) / g->graph->ground->height;
+		while (color[1] >= g->graph->ground->height)
+			color[1] -= g->graph->ground->height;
+		while (color[1] < 0)
+			color[1] += g->graph->ground->height;
+		while (color[0] >= g->graph->ground->width)
+			color[0] -= g->graph->ground->width;
+		while (color[0] < 0)
+			color[0] += g->graph->ground->width;
+		ft_set_pix(g->screen, x, y, ft_get_color(g->graph->ground, color[0], color[1]));
+		y++;
+	}
 }
 
 void	print_sprite_ray(t_game *g, int x, int height_wall, t_data *sp)
@@ -49,6 +82,7 @@ void	print_sprite_ray(t_game *g, int x, int height_wall, t_data *sp)
 		y[0]++;
 		true_y++;
 	}
+	ft_print_floor_texture(g, x, y[0]);
 }
 
 void	ft_print_ray(t_game *game)
